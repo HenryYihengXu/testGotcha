a6d7a920e1dcd1e97aeef1d049e1b7275d72b625
Yiheng Xu <yhxu@umd.edu>
2021-06-25 14:39:21 -0400

    adding write main

--- write-append-main.c
+++ write-append-main.c
@@ -4,10 +4,18 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <errno.h>
 
 int main() {
-    int fd = open("./a.txt", O_CREAT, S_IRWXU);
+    // int fd = open("./a.txt", O_APPEND);
+    int fd = open("./a.txt", O_RDWR | O_APPEND);
+    if (fd == -1) {
+        printf("open returned %d, errno: %d\n", fd, errno);
+    }
     char* buf = "aaaaaaaaa\n";
-    write(fd, buf, 10);
+    int ret = write(fd, buf, 10);
+    if (ret == -1) {
+        printf("write returned %d, errno: %d\n", ret, errno);
+    }
     return 0;
 }
