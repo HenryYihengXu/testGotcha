b0a9b9e6833d195e6da2111344b6be63b5712362
Yiheng Xu <yhxu@umd.edu>
2021-07-03 16:58:05 -0400

    testing calling gotcha init only once with shmget

--- gotcha-write-wrapper1.c
+++ gotcha-write-wrapper1.c
@@ -4,13 +4,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <errno.h>
 
 static gotcha_wrappee_handle_t wrappee_write_handle;
 static ssize_t gotcha_write_wrapper(int fd, const void *buf, size_t count);
 struct gotcha_binding_t write_wrap_actions [] = {
     {"write", gotcha_write_wrapper, &wrappee_write_handle}
 };
-int initialized = 0;
+int shm_id;
+void *shm_addr;
 
 int write1_init(int priority) {
     gotcha_set_priority("wrapper1", priority);
@@ -36,18 +40,36 @@ static void fini(void) __attribute__((destructor));
 
 static void init(void)
 {
-    write1_init(PRIORITY);
-    if (!initialized) {
-        fprintf(stderr, "write gotcha wrapper 1 initializing\n");
+    struct shmid_ds *shmctl_struct = (struct shmid_ds*) malloc(sizeof(struct shmid_ds));
+    shm_id = shmget(1, getpagesize(), IPC_CREAT | 0777);
+    if (shm_id == -1) {
+        fprintf(stderr, "shmget failed %d\n", errno);
+        return;
     }
-    // printf("write gotcha wrapper 1 initializing\n");
+    shm_addr = shmat(shm_id, 0, 0);
+    if (shm_addr == -1) {
+        fprintf(stderr, "shmat failed %d\n", errno);
+        return;
+    }
+    int res = shmctl(shm_id, IPC_STAT, shmctl_struct);
+    if (res == -1) {
+        fprintf(stderr, "shmctl failed %d\n", errno);
+        return;
+    }
+    if (shmctl_struct->shm_nattch <= 1) {
+        fprintf(stderr, "write gotcha wrapper 1 initializing\n");
+        write1_init(PRIORITY);
+    } else {
+        fprintf(stderr, "write gotcha wrapper 1 already initialized\n");
+    }
 }
 
 static void fini(void)
 {
-    if (!initialized) {
-        initialized = 1;
-        printf("write gotcha wrapper 1 finalizing\n");
+    int res = shmdt(shm_addr);
+    if (res == -1) {
+        fprintf(stderr, "shmdt failed %d\n", errno);
+        return;
     }
 }
 #endif
